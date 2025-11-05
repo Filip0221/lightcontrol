93b2fa982ec17656147474c07b4d3809be7f09b2
Filip Skup <filipskup231@gmail.com>
2025-11-05 14:58:58 +0100

    Initial Commit

---
+++ LightingControl/LightingControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightingControlApp.swift
+//  LightingControl
+//
+//  Created by Filip Skup on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct LightingControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
